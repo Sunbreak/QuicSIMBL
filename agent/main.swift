eb1d514861e67f796308ccca0d631888b6b2a2ee
Sunbreak <sunbreak.wang@gmail.com>
2023-08-23 10:56:28 +0800

    Use plain RunLoop instead of NSAppliction for QuicSIMBLAgent

--- agent/main.swift
+++ agent/main.swift
@@ -2,17 +2,8 @@ import Foundation
 import Cocoa
 import os.log
 
-class AppDelegate: NSObject, NSApplicationDelegate {
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        os_log("QuicSIMBLAgent didFinishLaunchingWithOptions")
-        NSWorkspace.shared.notificationCenter.addObserver(self, selector: #selector(didReceiveLaunch(notification:)), name: NSWorkspace.didLaunchApplicationNotification, object: nil)
-    }
-    
-    @objc func didReceiveLaunch(notification: Notification) {
-        os_log("QuicSIMBLAgent didReceiveLaunchNotification %{public}@", notification.userInfo!.description)
-    }
+NSWorkspace.shared.notificationCenter.addObserver(forName: NSWorkspace.didLaunchApplicationNotification, object: nil, queue: OperationQueue.main) { notification in
+    os_log("QuicSIMBLAgent didReceiveLaunchNotification %{public}@", notification.userInfo!.description)
 }
 
-NSApplication.shared.delegate = AppDelegate()
-
-_ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
+RunLoop.main.run()
